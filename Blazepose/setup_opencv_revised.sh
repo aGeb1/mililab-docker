8c1e00ce9e43bf991af20934745bf45f9fb5af13
Ari Gebhardt <ari.gebhardt@cooper.edu>
2022-07-14 19:16:09 -0400

    removed problematic if statement from setup_opencv_revised.sh

--- Blazepose/setup_opencv_revised.sh
+++ Blazepose/setup_opencv_revised.sh
@@ -8,55 +8,53 @@ fi
 opencv_build_file="$( cd "$(dirname "$0")" ; pwd -P )"/third_party/opencv_linux.BUILD
 workspace_file="$( cd "$(dirname "$0")" ; pwd -P )"/WORKSPACE
 
-if [ -z "$1" ]
-  then
-    echo "Installing OpenCV from source"
-    if [[ -x "$(command -v apt)" ]]; then
-      apt-get update && apt-get install -y build-essential git
-      apt-get install -y cmake ffmpeg libavformat-dev libdc1394-22-dev libgtk2.0-dev \
-                       libjpeg-dev libpng-dev libswscale-dev libtbb2 libtbb-dev \
-                       libtiff-dev
-    elif [[ -x "$(command -v dnf)" ]]; then
-      dnf update && dnf install cmake gcc gcc-c git
-      dnf install ffmpeg-devel libdc1394-devel gtk2-devel \
-                       libjpeg-turbo-devel libpng-devel tbb-devel \
-                       libtiff-devel
-    fi
-    rm -rf /tmp/build_opencv
-    mkdir /tmp/build_opencv
-    cd /tmp/build_opencv
-    git clone https://github.com/opencv/opencv_contrib.git
-    git clone https://github.com/opencv/opencv.git
-    mkdir opencv/release
-    cd opencv_contrib
-    git checkout 3.4
-    cd ../opencv
-    git checkout 3.4
-    cd release
-    cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=/usr/local \
-          -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_opencv_ts=OFF \
-          -DOPENCV_EXTRA_MODULES_PATH=/tmp/build_opencv/opencv_contrib/modules \
-          -DBUILD_opencv_aruco=OFF -DBUILD_opencv_bgsegm=OFF -DBUILD_opencv_bioinspired=OFF \
-          -DBUILD_opencv_ccalib=OFF -DBUILD_opencv_datasets=OFF -DBUILD_opencv_dnn=OFF \
-          -DBUILD_opencv_dnn_objdetect=OFF -DBUILD_opencv_dpm=OFF -DBUILD_opencv_face=OFF \
-          -DBUILD_opencv_fuzzy=OFF -DBUILD_opencv_hfs=OFF -DBUILD_opencv_img_hash=OFF \
-          -DBUILD_opencv_js=OFF -DBUILD_opencv_line_descriptor=OFF -DBUILD_opencv_phase_unwrapping=OFF \
-          -DBUILD_opencv_plot=OFF -DBUILD_opencv_quality=OFF -DBUILD_opencv_reg=OFF \
-          -DBUILD_opencv_rgbd=OFF -DBUILD_opencv_saliency=OFF -DBUILD_opencv_shape=OFF \
-          -DBUILD_opencv_structured_light=OFF -DBUILD_opencv_surface_matching=OFF \
-          -DBUILD_opencv_world=OFF -DBUILD_opencv_xobjdetect=OFF -DBUILD_opencv_xphoto=OFF \
-          -DCV_ENABLE_INTRINSICS=ON -DWITH_EIGEN=ON -DWITH_PTHREADS=ON -DWITH_PTHREADS_PF=ON \
-          -DWITH_JPEG=ON -DWITH_PNG=ON -DWITH_TIFF=ON
-    make -j 16
-    make install
-    rm -rf /tmp/build_opencv
-    echo "OpenCV has been built. You can find the header files and libraries in /usr/local/include/opencv2/ and /usr/local/lib"
-
-    # https://github.com/cggos/dip_cvqt/issues/1#issuecomment-284103343
-    touch /etc/ld.so.conf.d/mp_opencv.conf
-    bash -c  "echo /usr/local/lib >> /etc/ld.so.conf.d/mp_opencv.conf"
-    ldconfig -v
+echo "Installing OpenCV from source"
+if [[ -x "$(command -v apt)" ]]; then
+  apt-get update && apt-get install -y build-essential git
+  apt-get install -y cmake ffmpeg libavformat-dev libdc1394-22-dev libgtk2.0-dev \
+                   libjpeg-dev libpng-dev libswscale-dev libtbb2 libtbb-dev \
+                   libtiff-dev
+elif [[ -x "$(command -v dnf)" ]]; then
+  dnf update && dnf install cmake gcc gcc-c git
+  dnf install ffmpeg-devel libdc1394-devel gtk2-devel \
+                   libjpeg-turbo-devel libpng-devel tbb-devel \
+                   libtiff-devel
 fi
+rm -rf /tmp/build_opencv
+mkdir /tmp/build_opencv
+cd /tmp/build_opencv
+git clone https://github.com/opencv/opencv_contrib.git
+git clone https://github.com/opencv/opencv.git
+mkdir opencv/release
+cd opencv_contrib
+git checkout 3.4
+cd ../opencv
+git checkout 3.4
+cd release
+cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=/usr/local \
+      -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_opencv_ts=OFF \
+      -DOPENCV_EXTRA_MODULES_PATH=/tmp/build_opencv/opencv_contrib/modules \
+      -DBUILD_opencv_aruco=OFF -DBUILD_opencv_bgsegm=OFF -DBUILD_opencv_bioinspired=OFF \
+      -DBUILD_opencv_ccalib=OFF -DBUILD_opencv_datasets=OFF -DBUILD_opencv_dnn=OFF \
+      -DBUILD_opencv_dnn_objdetect=OFF -DBUILD_opencv_dpm=OFF -DBUILD_opencv_face=OFF \
+      -DBUILD_opencv_fuzzy=OFF -DBUILD_opencv_hfs=OFF -DBUILD_opencv_img_hash=OFF \
+      -DBUILD_opencv_js=OFF -DBUILD_opencv_line_descriptor=OFF -DBUILD_opencv_phase_unwrapping=OFF \
+      -DBUILD_opencv_plot=OFF -DBUILD_opencv_quality=OFF -DBUILD_opencv_reg=OFF \
+      -DBUILD_opencv_rgbd=OFF -DBUILD_opencv_saliency=OFF -DBUILD_opencv_shape=OFF \
+      -DBUILD_opencv_structured_light=OFF -DBUILD_opencv_surface_matching=OFF \
+      -DBUILD_opencv_world=OFF -DBUILD_opencv_xobjdetect=OFF -DBUILD_opencv_xphoto=OFF \
+      -DCV_ENABLE_INTRINSICS=ON -DWITH_EIGEN=ON -DWITH_PTHREADS=ON -DWITH_PTHREADS_PF=ON \
+      -DWITH_JPEG=ON -DWITH_PNG=ON -DWITH_TIFF=ON
+make -j 16
+make install
+rm -rf /tmp/build_opencv
+echo "OpenCV has been built. You can find the header files and libraries in /usr/local/include/opencv2/ and /usr/local/lib"
+
+# https://github.com/cggos/dip_cvqt/issues/1#issuecomment-284103343
+touch /etc/ld.so.conf.d/mp_opencv.conf
+bash -c  "echo /usr/local/lib >> /etc/ld.so.conf.d/mp_opencv.conf"
+ldconfig -v
+
 
 # Modify the build file.
 echo "Modifying MediaPipe opencv config"
